9fb32836522d8fcc86e3bdf76da76725fa3f0386
田所駿佑 <Shunsuke@tadokoroshunyuu-no-iMac.local>
2015-12-27 12:57:41 +0900

    use timing helper

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,12 @@
+
 (function ($) {
     "use strict";
     $(document).ready(function(){
         hljs.initHighlightingOnLoad();
     });
 }(jQuery));
+
+Handlebars.registerHelper('trimString', function(passedString, startstring, endstring) {
+   var theString = passedString.substring( startstring, endstring );
+   return new Handlebars.SafeString(theString)
+});
