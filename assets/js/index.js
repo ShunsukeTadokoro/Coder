be807fc2a7ae3f9b5c00d7bbf040b0f2b9085227
田所駿佑 <Shunsuke@tadokoroshunyuu-no-iMac.local>
2015-12-27 13:06:02 +0900

    fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,8 +5,3 @@
         hljs.initHighlightingOnLoad();
     });
 }(jQuery));
-
-Handlebars.registerHelper('trimString', function(passedString, startstring, endstring) {
-   var theString = passedString.substring( startstring, endstring );
-   return new Handlebars.SafeString(theString)
-});
